a12d6f608c9e162a0f21cf032f138e98897055a7
O'Brien <abucheriobrien@gmail.com>
2023-11-30 16:19:23 +0000

    Uncle

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+#include <stddef.h>
+
+/**
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+ *
+ * @first: Pointer to the first node
+ * @second: Pointer to the second node
+ *
+ * Return: Pointer to the lowest common ancestor, or NULL if not found
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	const binary_tree_t *ancestor = NULL;
+
+	if (!first || !second)
+		return (NULL);
+	while (first)
+	{
+		const binary_tree_t *temp = second;
+
+		while (temp)
+		{
+			if (temp == first)
+				return ((binary_tree_t *)first);
+			temp = temp->parent;
+		}
+		first = first->parent;
+	}
+	return ((binary_tree_t *)ancestor);
+}
